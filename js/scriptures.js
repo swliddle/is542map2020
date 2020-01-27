3d7766ae4958989470cd81b50d1c8038f47191ce
swliddle <liddle@byu.edu>
2020-01-27 13:47:57 -0700

    Worked on responding to hash changes.

--- js/scriptures.js
+++ js/scriptures.js
@@ -1,10 +1,19 @@
+/*============================================================================
+ * FILE:    scriptures.js
+ * AUTHOR:  Stephen W. Liddle
+ * DATE:    Winter 2020
+ *
+ * DESCRIPTION: Front-end JavaScript code for The Scriptures, Mapped.
+ *              IS 542, Winter 2020, BYU.
+ */
 /*jslint
     browser: true
     long: true */
 /*global console, XMLHttpRequest */
 /*property
-    books, forEach, hash, init, log, maxBookId, minBookId, onHashChanged,
-    onerror, onload, open, parse, push, response, send, status
+    books, forEach, getElementById, hash, id, init, innerHTML, length, log,
+    maxBookId, minBookId, onHashChanged, onerror, onload, open, parse, push,
+    response, send, slice, split, status
 */
 
 const Scriptures = (function () {
@@ -24,8 +33,12 @@ const Scriptures = (function () {
      *              PRIVATE METHOD DECLARATIONS
      */
     let ajax;
+    let bookChapterValid;
     let cacheBooks;
     let init;
+    let navigateBook;
+    let navigateChapter;
+    let navigateHome;
     let onHashChanged;
 
     /*------------------------------------------------------------------------
@@ -51,6 +64,20 @@ const Scriptures = (function () {
 
         request.onerror = failureCallback;
         request.send();
+    };
+
+    bookChapterValid = function (bookId, chapter) {
+        let book = books[bookId];
+
+        if (book === undefined || chapter < 0 || chapter > book.numChapters) {
+            return false;
+        }
+
+        if (chapter === 0 && book.numChapters > 0) {
+            return false;
+        }
+
+        return true;
     };
 
     cacheBooks = function (onInitializedCallback) {
@@ -95,8 +122,59 @@ const Scriptures = (function () {
         });
     };
 
+    navigateBook = function (bookId) {
+        console.log("navigateBook " + bookId);
+    };
+
+    navigateChapter = function (bookId, chapter) {
+        console.log("navigateChapter " + bookId + ", " + chapter);
+    };
+
+    navigateHome = function (volumeId) {
+        document.getElementById("scriptures").innerHTML =
+        "<div>The Old Testament</div>" +
+        "<div>The New Testament</div>" +
+        "<div>The Book of Mormon</div>" +
+        "<div>Doctrine and Covenants</div>" +
+        "<div>The Pearl of Great Price</div>" + volumeId;
+    };
+
     onHashChanged = function () {
-        console.log("The hash is " + location.hash);
+        let ids = [];
+
+        if (location.hash !== "" && location.hash.length > 1) {
+            ids = location.hash.slice(1).split(":");
+        }
+
+        if (ids.length <= 0) {
+            navigateHome();
+        } else if (ids.length === 1) {
+            let volumeId = Number(ids[0]);
+
+            if (volumeId < volumes[0].id || volumeId > volumes.slice(-1).id) {
+                navigateHome();
+            } else {
+                navigateHome(volumeId);
+            }
+        } else if (ids.length >= 2) {
+            let bookId = Number(ids[1]);
+
+            if (books[bookId] === undefined) {
+                navigateHome();
+            } else {
+                if (ids.length === 2) {
+                    navigateBook(bookId);
+                } else {
+                    let chapter = Number(ids[2]);
+
+                    if (bookChapterValid(bookId, chapter)) {
+                        navigateChapter(bookId, chapter);
+                    } else {
+                        navigateHome();
+                    }
+                }
+            }
+        }
     };
 
     /*------------------------------------------------------------------------
